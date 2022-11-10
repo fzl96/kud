a69d620dda26a2a2f4213b57f35f9b9b72590eb2
fzl96 <fpsdragon333@gmail.com>
2022-11-10 14:11:20 +0700

    change query

--- pages/api/categories/[id].ts
+++ pages/api/categories/[id].ts
@@ -2,20 +2,21 @@ import prisma from "@/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query
   if (req.method === "GET") {
     const category = await prisma.category.findUnique({
-      where: { id: req.query.id },
+      where: { id: id },
     });
     res.json(category);
   } else if (req.method === "PUT") {
     const category = await prisma.category.update({
-      where: { id: req.query.id },
+      where: { id: id },
       data: { name: req.body.name },
     });
     res.json(category);
   } else if (req.method === "DELETE") {
     const category = await prisma.category.delete({
-      where: { id: req.query.id },
+      where: { id: id },
     });
     res.json(category);
   } else {
